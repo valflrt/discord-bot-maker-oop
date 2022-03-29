8f5251f8620511a15ebc0b1596d14a386b94f893
valflrt <vfleurit@gmail.com>
2022-03-29 18:00:21 +0200

    add method "find"

--- src/CommandManager.ts
+++ src/CommandManager.ts
@@ -4,10 +4,27 @@ export interface CommandManagerConfig {
   commands: Command[];
 }
 
-class CommandManager {
+export default class CommandManager {
   public commands: Command[];
 
   constructor(config: CommandManagerConfig) {
     this.commands = config.commands;
+  }
+
+  /**
+   * Finds and returns a command in the list using a "command pattern"
+   * If the command isn't found, returns null
+   * @param pattern command call pattern
+   */
+  public find(pattern: string[]): Command | null {
+    const loop = (currentLevel: Command[] = [], i: number): Command | null => {
+      let command = currentLevel.find((c) => c.equals(pattern[i]));
+      if (i === pattern.length - 1 && command) return command!;
+      else if (command && command.commands.length !== 0) {
+        return loop(command.commands, i + 1);
+      } else return null;
+    };
+
+    return loop(this.commands, 0);
   }
 }
