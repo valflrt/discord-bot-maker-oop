5cc3f57df0c71f667ff25f4e0bed881a452f3032
valflrt <vfleurit@gmail.com>
2022-03-29 17:59:03 +0200

    edit Bot.ts

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,30 +1,45 @@
 import { Client, IntentsString } from "discord.js";
 
+import CommandManager from "./CommandManager";
+import Command from "./Command";
+
 import { AnyObject } from "./misc";
 
 export interface BotConfig {
   token: string;
+
+  prefix?: string;
   constants?: AnyObject;
 
   intents: IntentsString[];
+
+  commands?: Command[];
 }
 
 export default class Bot {
-  private token: string;
-
+  public prefix: string;
   public constants: AnyObject;
 
+  private token: string;
+
   private _client: Client;
-  private _commandManager: any;
+  private _commandManager: CommandManager;
 
   constructor(config: BotConfig) {
     this.token = config.token;
+
+    this.prefix = config.prefix ?? "!";
     this.constants = config.constants ?? {};
 
     this._client = new Client({ intents: config.intents });
+
+    this._commandManager = new CommandManager({
+      commands: config.commands ?? [],
+    });
   }
 
   public start() {
-    return;
+    this._client.login(this.token);
+    this._client.on("messageCreate", () => {});
   }
 }
