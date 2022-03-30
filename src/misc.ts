f2a37b906c8e16077ed199e8b22fd5ad5dcc7df3
valflrt <vfleurit@gmail.com>
2022-03-30 17:43:45 +0200

    add TimeStamp to misc.ts

--- src/misc.ts
+++ src/misc.ts
@@ -1,1 +1,23 @@
 export type AnyObject = { [key: string]: any };
+
+export class TimeStamp {
+  private time: number;
+
+  constructor() {
+    this.time = Date.now();
+  }
+
+  /**
+   * Resets the timestamp
+   */
+  public reset() {
+    this.time = Date.now();
+  }
+
+  /**
+   * Returns elapsed time from initialization (in milliseconds)
+   */
+  public get elapsedTime(): number {
+    return Date.now() - this.time;
+  }
+}
