7a6de19523e9b093b2e0d0846cc3bbbe03a2768d
valflrt <vfleurit@gmail.com>
2022-03-30 18:13:03 +0200

    add PluginManager

---
+++ src/PluginManager.ts
@@ -0,0 +1,13 @@
+import { Plugin, PluginConfig } from "./Plugin";
+
+export interface PluginManagerConfig {
+  plugins: PluginConfig[];
+}
+
+export class PluginManager {
+  public plugins: Plugin[];
+
+  constructor(config: PluginManagerConfig) {
+    this.plugins = config.plugins.map((p) => new Plugin(p));
+  }
+}
