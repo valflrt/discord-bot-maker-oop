5092abf79f0f720f259516763fdf027b5b44a3f0
val <vfleurit@gmail.com>
2022-03-31 11:46:42 +0000

    edit Bot.ts

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,18 +1,20 @@
-import { Client, IntentsString } from "discord.js";
+import { Client, IntentsString, MessageEmbed } from "discord.js";
 
-import { CommandManager } from "./CommandManager";
-import { CommandConfig } from "./Command";
+import { CommandManager } from "./commands/CommandManager";
+import { CommandConfig } from "./commands/Command";
 
-import { PluginManager } from "./PluginManager";
-import { Plugin, PluginConfig } from "./Plugin";
+import { PluginManager } from "./plugins/PluginManager";
+import { Plugin, PluginConfig } from "./plugins/Plugin";
+
+import { BotSettings, BotSettingsConfig } from "./BotSettings";
+import { Context } from "./Context";
 
 import { TimeStamp } from "./misc";
 import { AnyObject } from "./types";
 
 export interface BotConfig {
   token: string;
-
-  prefix?: string;
+  settings: BotSettingsConfig;
   constants?: AnyObject;
 
   intents: IntentsString[];
@@ -21,33 +23,29 @@ export interface BotConfig {
   plugins?: PluginConfig[];
 }
 
-export interface BotDeclaration {
-  prefix: string;
-  constants: AnyObject;
-
-  start: () => Promise<void>;
-}
-
-export class Bot implements BotDeclaration {
-  public prefix: string;
+export class Bot {
   public constants: AnyObject;
+
+  public settings: BotSettings;
 
   private token: string;
 
-  private _client: Client;
+  public client: Client;
+
   private _commandManager: CommandManager;
   private _pluginManager: PluginManager;
 
   constructor(config: BotConfig) {
     this.token = config.token;
 
-    this.prefix = config.prefix ?? "!";
     this.constants = config.constants ?? {};
 
-    this._client = new Client({ intents: config.intents });
+    this.settings = new BotSettings(config.settings);
+
+    this.client = new Client({ intents: config.intents });
 
     this._commandManager = new CommandManager({
-      prefix: this.prefix,
+      prefix: this.settings.prefix,
       commands: config.commands ?? [],
     });
 
@@ -59,12 +57,12 @@ export class Bot implements BotDeclaration {
   public async start() {
     console.log("Connecting...");
     let timestamp = new TimeStamp();
-    await this._client.login(this.token);
+    await this.client.login(this.token);
     console.log(`Successfully connected (in ${timestamp.elapsedTime}ms)`);
 
     console.log("Waiting for the bot to be ready...");
     timestamp.reset();
-    await new Promise((r) => this._client.on("ready", r));
+    await new Promise((r) => this.client.on("ready", r));
     console.log(`The bot is now ready (took ${timestamp.elapsedTime}ms)`);
 
     this._pluginManager.plugins.push(
@@ -75,7 +73,7 @@ export class Bot implements BotDeclaration {
             messageCreate: [
               (message) => {
                 // Skips if the message doesn't start with the prefix
-                if (!message.content.startsWith(this.prefix)) return;
+                if (!message.content.startsWith(this.settings.prefix)) return;
 
                 let command = this._commandManager.parseTextAndFindCommand(
                   message.content
@@ -84,9 +82,11 @@ export class Bot implements BotDeclaration {
                 if (!command) return;
 
                 command.execute(
-                  {
+                  new Context({
                     message,
-                  },
+                    command,
+                    prefix: this.settings.prefix,
+                  }),
                   this
                 );
               },
