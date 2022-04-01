56552d26ee4b4bb5e2ed5dd0c5020b3755f6d865
valflrt <vfleurit@gmail.com>
2022-04-01 19:46:57 +0200

    edit PluginManager.ts - add methods "add" and "setupClient"

--- src/plugins/PluginManager.ts
+++ src/plugins/PluginManager.ts
@@ -1,17 +1,47 @@
-import { Plugin, PluginConfig } from "./Plugin";
+import { Client, ClientEvents } from "discord.js";
 
-export interface PluginManagerConfig {
-  plugins: PluginConfig[];
+import { Plugin, PluginEventListenerNotGeneric, PluginSetup } from "./Plugin";
+
+import { Base, BaseConfig } from "../Base";
+
+export interface PluginManagerConfig extends BaseConfig {
+  plugins: PluginSetup[];
 }
 
-export class PluginManager {
-  public plugins: Plugin[];
+export class PluginManager extends Base {
+  public plugins: Plugin[] = [];
 
   constructor(config: PluginManagerConfig) {
-    this.plugins = config.plugins.map((p) => new Plugin(p));
+    super(config);
+    config.plugins.forEach((p) => this.add(p));
   }
 
   /**
-   * TODO: Add client setup method
+   * Adds a plugin from a PluginSetup
+   * @param setup plugin setup
    */
+  public add(setup: PluginSetup) {
+    this.plugins.push(new Plugin(setup(this.bot)));
+  }
+
+  /**
+   * Sets up a client: add all the event listeners of the plugin to the client
+   * @param client client to set up
+   */
+  public setupClient(client: Client) {
+    return new Promise<void>((resolve) => {
+      this.plugins.forEach((p) => {
+        p.iterateThroughListeners((eventType, eventName, listener) => {
+          let args: [keyof ClientEvents, PluginEventListenerNotGeneric] = [
+            eventName,
+            listener as PluginEventListenerNotGeneric,
+          ];
+          if (eventType === "on") client.on(...args);
+          else if (eventType === "once") client.once(...args);
+          else if (eventType === "off") client.off(...args);
+        });
+      });
+      resolve();
+    });
+  }
 }
