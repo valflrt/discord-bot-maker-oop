b64190e79e8f43bae200956dd75b2623b182cf26
valflrt <vfleurit@gmail.com>
2022-04-05 19:36:05 +0200

    remove build/

--- build/Base.d.ts
+++
@@ -1,8 +0,0 @@
-import { Bot } from "./Bot";
-export interface BaseConfig {
-    bot: Bot;
-}
-export declare class Base {
-    bot: Bot;
-    constructor(config: BaseConfig);
-}
--- build/Base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Base = void 0;
-class Base {
-    constructor(config) {
-        this.bot = config.bot;
-    }
-}
-exports.Base = Base;
